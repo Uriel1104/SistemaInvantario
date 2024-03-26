eb654b29f2fba6b144766677e085e6fdd221f314
UrielMendez <urielmendez03yby@gmail.com>
2024-03-25 21:17:37 -0600

    Configuracion del proyecto

---
+++ SistemaInventario.AccesoDatos/Data/Migrations/20240321194058_AgregarMigracionInicial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaInventario.Data.Migrations
+{
+    public partial class AgregarMigracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
